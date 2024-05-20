bffbe2c11844a313a6fb347f0438f0ab68fc8b60
Rachid El-ismaiyly <rel-isma@e1r6p14.1337.ma>
2024-05-20 15:55:31 +0100

    first run

--- srcs/requirements/mariadb/tools/creating_db.sh
+++ srcs/requirements/mariadb/tools/creating_db.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-WORDPRESS_DB_NAME="wordpress_db"
-WORDPRESS_DB_USER="wp_user"
-WORDPRESS_DB_PASSWORD="password"
-
 # Start MariaDB service
 service mariadb start
 
 until mysqladmin ping -hlocalhost --silent; do
-    sleep 5
+    sleep 3
 done
 
 # Allow external connections
@@ -16,9 +12,9 @@ echo "bind-address = 0.0.0.0" >> /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # Set up the database and user
 mysql -u root <<MYSQL_SCRIPT
-CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME};
-CREATE USER IF NOT EXISTS '${WORDPRESS_DB_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${WORDPRESS_DB_USER}'@'%';
+CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_NAME;
+CREATE USER IF NOT EXISTS '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'%';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
--- srcs/requirements/wordpress/tools/installing_wp_cli.sh
+++ srcs/requirements/wordpress/tools/installing_wp_cli.sh
@@ -15,9 +15,18 @@ wait_for_mariadb
 
 # Install WordPress
 wp core download --allow-root
-wp config create --dbname=wordpress_db --dbuser=wp_user --dbpass=password --dbhost=mariadb:3306 --allow-root
-wp core install --url=https://localhost:8443 --title="My WordPress Site" --admin_user=admin --admin_password=admin --admin_email=codeing.ch@gmail.com --allow-root
-wp user create rel-isma relismaiyly@gmail.com --role=author --user_pass=2001 --allow-root
+
+# creating file wp-config.php
+wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER \
+  --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=mariadb:3306 --allow-root
+
+
+# install wordpress
+wp core install --url=$WORDPRESS_URL --title="$WORDPRESS_TITLE" \
+  --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+
+# Create WordPress user
+wp user create $WP_USER_NAME $WP_USER_EMAIL --role=$WP_USER_ROLE --user_pass=$WP_USER_PASSWORD --allow-root
 
 chown -R www-data:www-data /var/www/html
 
